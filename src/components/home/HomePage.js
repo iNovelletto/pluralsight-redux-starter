43f54dff545fcc34bb8b4051cdbf4bb6e5c3df81
igomes <igomes@rdisoftware.com>
2017-11-01 18:43:23 -0200

    Async in Redux

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,7 +5,7 @@ class HomePage extends React.Component {
   render() {
       return (
         <div className="jumbotron">
-          <h1>Administration</h1>
+          <h1>XPTO</h1>
           <p>Test React, Redux, React Router in ES6</p>
           <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
         </div>
