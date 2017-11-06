0fa2792bb32c499481507d0aa48e1f37ff82ac29
igomes <igomes@rdisoftware.com>
2017-11-06 14:58:32 -0200

    Async Status and Error Handling

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import { browserHistory } from 'react-router';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import authors from './authorReducer';
 import courses from './courseReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   authors: authors,
-  courses: courses
+  courses: courses,
+  ajaxCallsInProgress: ajaxCallsInProgress
 });
 
 export default rootReducer;
